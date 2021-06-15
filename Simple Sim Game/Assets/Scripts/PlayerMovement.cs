c2a7746e06a54bbcc5fbc3389b9bd853df33a432
hilarykseth <69688591+hilarykseth@users.noreply.github.com>
2021-06-15 20:03:16 +0300

    Added player animation

--- Simple Sim Game/Assets/Scripts/PlayerMovement.cs
+++ Simple Sim Game/Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,40 @@ public class PlayerMovement : MonoBehaviour
     
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        PlayerDirection();
     }
 
     void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerDirection()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        PlayerAnimation();
+    }
+
+    void PlayerAnimation()
+    {
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void PlayerMove()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
