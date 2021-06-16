561b1ed80607e267a0f8a84e09cf5e205a6fbaf1
hilarykseth <69688591+hilarykseth@users.noreply.github.com>
2021-06-16 07:54:32 +0300

    Added trigger for instance swtiching

---
+++ Simple Sim Game/Assets/Scripts/InstanceEntrance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstanceEntrance : MonoBehaviour
+{
+
+    /*void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Indoor");
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Indoor");
+        }
+    }
+}
